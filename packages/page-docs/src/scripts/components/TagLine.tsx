b2eb9c9a2a24cf55366e46bfaeebfa203577d9f7
Florian Rappl <florian.rappl@smapiot.com>
2019-04-11 01:08:08 +0200

    Design adjustments and added version to documentation #16

--- packages/page-docs/src/scripts/components/TagLine.tsx
+++ packages/page-docs/src/scripts/components/TagLine.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
 
-export const TagLine: React.SFC = () => (
+export interface TagLineProps {
+  version?: string;
+}
+
+export const TagLine: React.SFC<TagLineProps> = ({ version }) => (
   <div className="tagline">
     <p>Easily build a next generation portal application</p>
+    <div className="meta">
+      {version && (
+        <>
+          <i className="fas fa-code-branch" /> v{version}{' '}
+        </>
+      )}
+    </div>
   </div>
 );
