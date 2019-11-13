a26526f09681c4633fa5370eb264cad40cc2da55
Florian Rappl <florian.rappl@smapiot.com>
2019-11-13 14:30:34 +0100

    Improved comparison

--- src/packages/piral-core/src/utils/compare.ts
+++ src/packages/piral-core/src/utils/compare.ts
@@ -1,18 +1,29 @@
-export function compare<T>(a: T, b: T): boolean {
+function compareObjects(a: any, b: any) {
+  for (const i in a) {
+    if (!(i in b)) {
+      return false;
+    }
+  }
+
+  for (const i in b) {
+    if (!compare(a[i], b[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function compare<T>(a: T, b: T) {
   if (a !== b) {
-    let i: any;
+    const ta = typeof a;
+    const tb = typeof b;
 
-    for (i in a) {
-      if (!(i in b)) {
-        return false;
-      }
+    if (ta === tb && ta === 'object' && a && b) {
+      return compareObjects(a, b);
     }
 
-    for (i in b) {
-      if (!compare(a[i], b[i])) {
-        return false;
-      }
-    }
+    return false;
   }
 
   return true;
