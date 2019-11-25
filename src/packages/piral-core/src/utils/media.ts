6b0ec3640ead5db0e9d131217bdf355ad74e840e
Florian Rappl <florian.rappl@smapiot.com>
2019-11-25 10:02:49 +0100

    Allow usage on server-side

--- src/packages/piral-core/src/utils/media.ts
+++ src/packages/piral-core/src/utils/media.ts
@@ -4,8 +4,10 @@ export const defaultLayouts: LayoutTypes = ['desktop', 'tablet', 'mobile'];
 
 export const defaultBreakpoints: LayoutBreakpoints = ['(min-width: 991px)', '(min-width: 481px)', '(max-width: 480px)'];
 
+const mm = typeof window === 'undefined' ? () => ({ matches: [] }) : window.matchMedia;
+
 export function getCurrentLayout<T>(breakpoints: Array<string>, layouts: Array<T>, defaultLayout: T) {
-  const query = breakpoints.findIndex(q => matchMedia(q).matches);
+  const query = breakpoints.findIndex(q => mm(q).matches);
   const layout = layouts[query];
   return layout !== undefined ? layout : defaultLayout;
 }
