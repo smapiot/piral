b23724f27bc5ec384c0f2c1bbb6766aaf32b5596
Florian Rappl <florian.rappl@smapiot.com>
2019-12-09 02:46:31 +0100

    Fixed bug with orphan targets

--- src/packages/piral-cli/src/apps/build-piral.ts
+++ src/packages/piral-cli/src/apps/build-piral.ts
@@ -92,7 +92,7 @@ async function bundleFiles(
 
 async function generateDeclaration(outDir: string, root: string, name: string, dependencies: Record<string, string>) {
   const declaration = combineApiDeclarations(root, Object.keys(dependencies));
-  const result = await declarationFlattening(outDir, name, declaration);
+  const result = await declarationFlattening(root, name, declaration);
   await createFileIfNotExists(outDir, 'index.d.ts', result);
 }
 
