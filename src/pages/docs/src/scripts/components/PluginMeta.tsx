104431a169c589745fb554e4b5daafb4e3935c7f
Florian Rappl <florian.rappl@smapiot.com>
2019-12-10 17:55:12 +0100

    Improved the plugin meta display

--- src/pages/docs/src/scripts/components/PluginMeta.tsx
+++ src/pages/docs/src/scripts/components/PluginMeta.tsx
@@ -12,21 +12,73 @@ export interface PluginMetaProps {
   license: string;
 }
 
-export const PluginMeta: React.FC<PluginMetaProps> = ({ description, keywords, author, homepage, license }) => (
-  <div>
-    <b>Description:</b>
-    <p>{description}</p>
-    <b>Author:</b>
-    <p>{author}</p>
-    <b>Homepage:</b>
-    <p>{homepage}</p>
-    <b>License:</b>
-    <p>{license}</p>
-    <b>Keywords:</b>
-    <ul className="keywords">
-      {keywords.map(keyword => (
-        <li key={keyword}>{keyword}</li>
-      ))}
-    </ul>
-  </div>
-);
+export const PluginMeta: React.FC<PluginMetaProps> = ({
+  description,
+  keywords,
+  author,
+  homepage,
+  license,
+  dependencies = {},
+  devDependencies = {},
+}) => {
+  const dependencyNames = Object.keys(dependencies);
+  const devDependencyNames = Object.keys(devDependencies);
+  const hasDependencies = dependencyNames.length + devDependencyNames.length > 0;
+
+  return (
+    <div>
+      <p>{description}</p>
+      <h4>Author</h4>
+      <p>{author}</p>
+      <h4>Homepage</h4>
+      <p>
+        <a href={homepage} target="_blank">
+          {homepage}
+        </a>
+      </p>
+      <h4>License</h4>
+      <p>
+        <a href={`https://opensource.org/licenses/${license}`} target="_blank">
+          {license}
+        </a>
+      </p>
+      <h4>Keywords</h4>
+      <ul className="keywords">
+        {keywords.map(keyword => (
+          <li key={keyword}>{keyword}</li>
+        ))}
+      </ul>
+      <h4>Dependencies</h4>
+      {hasDependencies ? (
+        <>
+          {dependencyNames.length > 0 && (
+            <>
+              <p>The package comes with the following dependencies:</p>
+              <ul className="keywords">
+                {dependencyNames.map(name => (
+                  <li key={name}>
+                    <b>{name}</b>: {dependencies[name]}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+          {devDependencyNames.length > 0 && (
+            <>
+              <p>The package was developed using the following dependencies:</p>
+              <ul className="keywords">
+                {devDependencyNames.map(name => (
+                  <li key={name}>
+                    <b>{name}</b>: {devDependencies[name]}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </>
+      ) : (
+        <p>No dependencies.</p>
+      )}
+    </div>
+  );
+};
