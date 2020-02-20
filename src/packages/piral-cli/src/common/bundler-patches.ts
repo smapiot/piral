8f9f5cc66e9589c2ee3e6e66297fbdad4f205a55
Florian Rappl <florian.rappl@smapiot.com>
2020-02-20 09:44:13 +0100

    Added support for ketting #160

--- src/packages/piral-cli/src/common/bundler-patches.ts
+++ src/packages/piral-cli/src/common/bundler-patches.ts
@@ -1,13 +1,27 @@
 import { readText, writeText } from './io';
+import { resolve } from 'path';
+
+async function replaceAll(dir: string, file: string, original: string, replacement: string) {
+  const content = await readText(dir, file);
+
+  if (content && content.indexOf(original) !== -1) {
+    await writeText(dir, file, content.split(original).join(replacement));
+  }
+}
 
 const patchMap: Record<string, (rootDir: string) => Promise<void>> = {
   async buffer(rootDir) {
-    const gn = ' global.';
-    const content = await readText(rootDir, 'index.js');
-
-    if (content && content.indexOf(gn) !== -1) {
-      await writeText(rootDir, 'index.js', content.split(gn).join(' window.'));
-    }
+    await replaceAll(rootDir, 'index.js', ' global.', ' window.');
+  },
+  async 'has-symbols'(rootDir) {
+    await replaceAll(rootDir, 'index.js', ' global.', ' window.');
+  },
+  async ketting(rootDir) {
+    const util = resolve(rootDir, 'dist', 'util');
+    const utils = resolve(rootDir, 'dist', 'utils');
+    await replaceAll(util, 'fetch-helper.js', 'global', 'window');
+    await replaceAll(utils, 'fetch-helper.js', 'global', 'window');
+    await replaceAll(utils, 'fetch-polyfill.js', 'global', 'window');
   },
 };
 
