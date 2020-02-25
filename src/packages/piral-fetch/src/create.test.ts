02f6acee69a5cd2fa75211b0e96c6bff93e0c985
Florian Rappl <florian.rappl@smapiot.com>
2020-02-25 16:39:25 +0100

    Use local setup for testing

--- src/packages/piral-fetch/src/create.test.ts
+++ src/packages/piral-fetch/src/create.test.ts
@@ -3,24 +3,55 @@ import 'whatwg-fetch';
 import { createFetchApi } from './create';
 
 describe('Create fetch API Module', () => {
+  let terminate = () => {};
+
+  beforeAll(() => {
+    const express = require('express');
+    const cors = require('cors');
+    const app = express();
+
+    app.use(cors());
+
+    app.get('/json', (_, res) => {
+      res.json([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+
+    app.get('/xml', (_, res) => {
+      res.set('Content-Type', 'text/xml');
+      res.send(
+        Buffer.from(`<?xml version="1.0" encoding="UTF-8"?>
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>`),
+      );
+    });
+
+    terminate = app.listen(3000);
+  });
+
+  afterAll(() => terminate());
+
   it('works with default options against a JSON API', async () => {
     const context = { emit: jest.fn() } as any;
     const { fetch } = createFetchApi({
-      base: 'https://jsonplaceholder.typicode.com',
+      base: 'http://localhost:3000',
     })(context) as any;
-    const response = await fetch('users');
+    const response = await fetch('json');
     const result = response.body;
     expect(Array.isArray(result)).toBeTruthy();
     expect(result.length).toBe(10);
-    expect(result[0].name).toBe('Leanne Graham');
+    expect(result[0]).toBe(1);
   });
 
   it('interprets the result as text if explicitly used despite JSON API', async () => {
     const context = { emit: jest.fn() } as any;
     const { fetch } = createFetchApi({
-      base: 'https://jsonplaceholder.typicode.com',
+      base: 'http://localhost:3000',
     })(context) as any;
-    const response = await fetch('users', { result: 'text' });
+    const response = await fetch('json', { result: 'text' });
     const result = response.body;
     expect(typeof result).toBe('string');
     expect(Array.isArray(JSON.parse(result))).toBeTruthy();
@@ -29,9 +60,9 @@ describe('Create fetch API Module', () => {
   it('has the correct response code', async () => {
     const context = { emit: jest.fn() } as any;
     const { fetch } = createFetchApi({
-      base: 'https://jsonplaceholder.typicode.com',
+      base: 'http://localhost:3000',
     })(context) as any;
-    const response = await fetch('users');
+    const response = await fetch('json');
     const result = response.code;
     expect(result).toBe(200);
   });
@@ -39,10 +70,10 @@ describe('Create fetch API Module', () => {
   it('works with default options against a non-JSON API', async () => {
     const context = { emit: jest.fn() } as any;
     const { fetch } = createFetchApi({
-      base: 'https://cdn.animenewsnetwork.com/encyclopedia/',
+      base: 'http://localhost:3000',
     })(context) as any;
-    const response = await fetch('api.xml?anime=4658');
+    const response = await fetch('xml');
     const result = response.body;
-    expect(result.substr(0, 5)).toBe(`<ann>`);
+    expect(result.substr(0, 5)).toBe(`<?xml`);
   });
 });
