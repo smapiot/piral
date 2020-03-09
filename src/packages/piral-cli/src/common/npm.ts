4fdd44ece9b0e6ba72bbe445c131f345460f0ad8
Florian Rappl <florian.rappl@smapiot.com>
2020-03-09 13:30:28 +0100

    Improved output from npm

--- src/packages/piral-cli/src/common/npm.ts
+++ src/packages/piral-cli/src/common/npm.ts
@@ -56,24 +56,28 @@ export function isGitPackage(fullName: string) {
 export async function installDependencies(target = '.', ...flags: Array<string>) {
   const ms = new MemoryStream();
   await runNpmProcess(['install', ...flags], target, ms);
+  log('generalDebug_0003', `NPM install dependencies result: ${ms.value}`);
   return ms.value;
 }
 
 export async function installPackage(packageRef: string, target = '.', ...flags: Array<string>) {
   const ms = new MemoryStream();
   await runNpmProcess(['install', packageRef, ...flags], target, ms);
+  log('generalDebug_0003', `NPM install package result: ${ms.value}`);
   return ms.value;
 }
 
 export async function createPackage(target = '.', ...flags: Array<string>) {
   const ms = new MemoryStream();
   await runNpmProcess(['pack', ...flags], target, ms);
+  log('generalDebug_0003', `NPM pack result: ${ms.value}`);
   return ms.value;
 }
 
 export async function findLatestVersion(packageName: string) {
   const ms = new MemoryStream();
   await runNpmProcess(['show', packageName, 'version'], '.', ms);
+  log('generalDebug_0003', `NPM show result: ${ms.value}`);
   return ms.value;
 }
 
