c0bf7bad7d5a6fb627c61801c7cf97ef3a3cbab6
Florian Rappl <florian.rappl@smapiot.com>
2020-04-06 00:21:50 +0200

    Added tests

--- src/packages/piral-core/src/actions/state.test.ts
+++ src/packages/piral-core/src/actions/state.test.ts
@@ -1,7 +1,39 @@
+import { Atom, deref } from '@dbeining/react-atom';
 import { readState, dispatch } from './state';
 
-describe('Piral-Core state actions', () => {
-  it('readState gets the current state', () => {});
+function createMockContainer(initialState = {}) {
+  const state = Atom.of(initialState);
+  return {
+    context: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+      defineActions() {},
+      state,
+      read() {
+        return deref(state);
+      }
+    } as any,
+  };
+}
 
-  it('dispatch updates the current state', () => {});
+describe('Piral-Core state actions', () => {
+  it('readState gets the current state', () => {
+    const { context } = createMockContainer({
+      test: 42,
+    });
+    const result = readState(context, (state: any) => state.test);
+    expect(result).toBe(42);
+  });
+
+  it('dispatch updates the current state', () => {
+    const { context } = createMockContainer({
+      test: 0,
+    });
+    const result = dispatch(context, (state: any) => ({
+      ...state,
+      test: 11,
+    }));
+    expect(context.read().test).toBe(11);
+  });
 });
