eeb85280d8b6c1975c043abb12dd2436ed409fe5
Florian Rappl <florian.rappl@smapiot.com>
2020-05-29 00:49:55 +0200

    Improved support for piral-native

---
+++ src/packages/piral-core/dependencies.codegen.native.js
@@ -0,0 +1,2 @@
+// required for piral-native; otherwise non-essential
+module.exports = function() {};
--- src/packages/piral-native/src/api.ts
+++ src/packages/piral-native/src/api.ts
@@ -1,4 +1,5 @@
 import { Extend, createInstance, PiralConfiguration } from 'piral-core';
+import { NativeRouter } from 'react-router-native';
 
 function extendPiralApi(customApis: Extend | Array<Extend> = []) {
   return Array.isArray(customApis) ? customApis : [customApis];
@@ -11,6 +12,22 @@ function extendPiralApi(customApis: Extend | Array<Extend> = []) {
 export function createNativePiral(config: PiralConfiguration = {}) {
   return createInstance({
     ...config,
+    state: {
+      ...config.state,
+      components: {
+        LoadingIndicator: () => null,
+        Router: NativeRouter,
+        ...config.state?.components,
+      },
+    },
+    getDependencies(meta) {
+      const deps = config.getDependencies?.(meta) ?? {};
+      return {
+        'react-native': require('react-native'),
+        'react-router-native': require('react-native'),
+        ...deps,
+      };
+    },
     extendApi: extendPiralApi(config.extendApi),
   });
 }
