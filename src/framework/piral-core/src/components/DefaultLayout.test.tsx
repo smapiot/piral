ea32a5d0a9099eae95e3edfc5239674183116d85
iotcloudarchitect <40333373+iotcloudarchitect@users.noreply.github.com>
2020-07-02 16:20:15 +0200

    Update DefaultLayout.test.tsx

--- src/framework/piral-core/src/components/DefaultLayout.test.tsx
+++ src/framework/piral-core/src/components/DefaultLayout.test.tsx
@@ -8,20 +8,22 @@ describe('Default Layout Component', () => {
       const node = mount(<DefaultLayout currentLayout="desktop" children={children}/>);
       expect(node.find('div').length).toBe(1);
     });
+
     it('renders correctly for mobile', () => {
         const children = <div></div>;
         const node = mount(<DefaultLayout currentLayout="mobile" children={children}/>);
         expect(node.find('div').length).toBe(1);
-      });
-      it('renders correctly for tablet', () => {
-        const children = <div></div>;
-        const node = mount(<DefaultLayout currentLayout="tablet" children={children}/>);
-        expect(node.find('div').length).toBe(1);
-      });
-      it('rendering reports error because of wrong element', () => {
-        const children = <div></div>;
-        const node = mount(<DefaultLayout currentLayout="tablet" children={<div></di>}/>);
-        expect(node).toBeNull;
-      });
-  });
-  
+    });
+
+    it('renders correctly for tablet', () => {
+      const children = <div></div>;
+      const node = mount(<DefaultLayout currentLayout="tablet" children={children}/>);
+      expect(node.find('div').length).toBe(1);
+    });
+
+    it('rendering reports error because of wrong element', () => {
+      const children = <div></div>;
+      const node = mount(<DefaultLayout currentLayout="tablet" children={<div></di>}/>);
+      expect(node).toBeNull();
+    });
+});  
