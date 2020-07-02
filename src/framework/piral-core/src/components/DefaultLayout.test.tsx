bade0df4ad783a0afa7625d929c25d7e839ec8ee
Jens Thirmeyer <jens@thirmeyer.de>
2020-07-02 14:40:37 +0200

    Added new test

---
+++ src/framework/piral-core/src/components/DefaultLayout.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { DefaultLayout } from './DefaultLayout';
+import { mount } from 'enzyme';
+
+describe('Default Layout Component', () => {
+    it('renders correctly for desktop', () => {
+      const children = <div></div>;
+      const node = mount(<DefaultLayout currentLayout="desktop" children={children}/>);
+      expect(node.find('div').length).toBe(1);
+    });
+    it('renders correctly for mobile', () => {
+        const children = <div></div>;
+        const node = mount(<DefaultLayout currentLayout="mobile" children={children}/>);
+        expect(node.find('div').length).toBe(1);
+      });
+      it('renders correctly for tablet', () => {
+        const children = <div></div>;
+        const node = mount(<DefaultLayout currentLayout="tablet" children={children}/>);
+        expect(node.find('div').length).toBe(1);
+      });
+      it('rendering reports error because of wrong element', () => {
+        const children = <div></div>;
+        const node = mount(<DefaultLayout currentLayout="tablet" children={<div></di>}/>);
+        expect(node).toBeNull;
+      });
+  });
+  
