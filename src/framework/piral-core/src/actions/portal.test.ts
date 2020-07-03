5db81f0f3405aab9605255477365344b6c99d1b7
Jens Thirmeyer <jens@thirmeyer.de>
2020-07-03 13:15:43 +0200

    Added portal tests

--- src/framework/piral-core/src/actions/portal.test.ts
+++ src/framework/piral-core/src/actions/portal.test.ts
@@ -1,24 +1,48 @@
 import * as React from 'react';
-import { Atom, swap } from '@dbeining/react-atom';
+import { Atom, swap, deref } from '@dbeining/react-atom';
 import { showPortal, destroyPortal } from './portal';
 
 describe('Piral-Core portal actions', () => {
-  it('showPortal', () => {
-    const children = <div></div>;
-    const portal: React.ReactPortal = { key: 'test', children: children } as any;
+  it('showPortal adds a portal', () => {
+    const children = React.createElement('div');
+    const newPortal: React.ReactPortal = { key: 'test', children: { children }, type: 'div', props: null };
+    const portal: React.ReactPortal = { key: 'toast', children: { children }, type: 'div', props: null };
+
     const state = Atom.of({
-      portals: { test: [] },
+      portals: { p1: { portal } },
     });
+
     const ctx: any = {
       state,
       dispatch(update) {
         swap(state, update);
       },
     };
-    showPortal(ctx, 'test', portal);
-    expect(ctx.state).not.toBeNull();
-    console.log(ctx);
+
+    showPortal(ctx, 'test', newPortal);
+    const { portals } = deref(ctx.state);
+    expect(portals).not.toBeNull();
+    expect(portals.test).not.toBeNull();
   });
 
-  
+  it('destroyPortal removes a portal', () => {
+    const children = React.createElement('div');
+    const portal: React.ReactPortal = { key: 'test', children: { children }, type: 'div', props: null };
+
+    const state = Atom.of({
+      portals: { test: { portal } },
+    });
+
+    const ctx: any = {
+      state,
+      dispatch(update) {
+        swap(state, update);
+      },
+    };
+
+    destroyPortal(ctx, 'test');
+    const { portals } = deref(ctx.state);
+    expect(portals).not.toBeNull();
+    expect(portals.test).toBeUndefined();
+  });
 });
