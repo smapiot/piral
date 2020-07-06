24ae7704ba8680c6821893d23d77f5602f376cfc
Finn Fiedler <finn.fiedler@smapiot.com>
2020-07-06 16:43:53 +0200

    improved strategies.test.tsx code coverage

--- src/framework/piral-core/src/helpers.test.tsx
+++ src/framework/piral-core/src/helpers.test.tsx
@@ -1,7 +1,6 @@
 import { createPiletOptions, PiletOptionsConfig } from './helpers';
 import { globalDependencies } from './modules';
-import { GlobalStateContext } from './types';
-import { PiletApiCreator, PiletMetadata, Pilet } from 'piral-base';
+import { PiletMetadata, Pilet } from 'piral-base';
 import { Atom, swap } from '@dbeining/react-atom';
 
 function createMockApi(meta: PiletMetadata){
@@ -120,16 +119,6 @@ describe('Piral-Core helpers module', () => {
   });
 
   it('createPiletOptions creates the options with provided requestPilets', () => {
-    // const requestPilets = jest.fn();
-    // const options = createPiletOptions({
-    //   availablePilets: [],
-    //   context: {},
-    //   createApi: () => ({}),
-    //   requestPilets,
-    // });
-    // expect(requestPilets).not.toHaveBeenCalled();
-    // options.fetchPilets();
-    // expect(requestPilets).toHaveBeenCalled();
 
     const wasUndefined = process.env.DEBUG_PIRAL === undefined;
 
