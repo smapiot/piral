196f7b856d9adb0a43ba0a85bdab8f73efdf68ed
Finn Fiedler <finn.fiedler@smapiot.com>
2020-07-06 14:48:05 +0200

    creates tests for helpers.tsx

--- src/framework/piral-core/src/helpers.test.tsx
+++ src/framework/piral-core/src/helpers.test.tsx
@@ -1,35 +1,177 @@
-import { createPiletOptions } from './helpers';
+import { createPiletOptions, PiletOptionsConfig } from './helpers';
 import { globalDependencies } from './modules';
+import { GlobalStateContext } from './types';
+import { PiletApiCreator, PiletMetadata, Pilet } from 'piral-base';
+import { Atom, swap } from '@dbeining/react-atom';
+
+function createMockApi(meta: PiletMetadata){
+  return {
+    meta,
+    emit: jest.fn(),
+    off: jest.fn(),
+    on: jest.fn(),
+  }as any;
+}
+
+function createMockContainer() {
+  const state = Atom.of({
+    components: {},
+  });
+  return {
+    context: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+      state,
+      setComponent(name, comp) {
+        swap(state, s => ({
+          ...s,
+          components: {
+            ...s.components,
+            [name]: comp,
+          },
+        }));
+      },
+    } as any,
+  };
+}
 
 describe('Piral-Core helpers module', () => {
   it('createPiletOptions creates the options using the provided pilets', () => {
-    const options = createPiletOptions({
-      availablePilets: [],
-      context: {},
-      createApi: () => ({}),
-    });
-    expect(options.pilets).toEqual([]);
+
+    const wasUndefined = process.env.DEBUG_PIRAL === undefined;
+
+    process.env.DEBUG_PIRAL = "true";
+
+    // Arrange
+    const setupMock = jest.fn();
+    const globalContext = createMockContainer().context
+    const providedPilets: Array<Pilet> = [
+      {
+        setup: setupMock,
+        hash: '12g',
+        name: 'somePilet',
+        version: '1',
+      },
+      {
+        setup: setupMock,
+        hash: '99a',
+        name: 'anotherPilet',
+        version: '2',
+      },
+    ];
+    const optionsConfig: PiletOptionsConfig = {
+      createApi: createMockApi,
+      availablePilets: providedPilets,
+      context: globalContext,
+      fetchDependency: jest.fn(),
+      getDependencies: jest.fn(),
+      loadPilet: jest.fn(),
+      requestPilets: jest.fn(() => Promise.resolve(providedPilets)),
+      strategy: jest.fn()
+    }
+
+    // Act
+    const options = createPiletOptions(optionsConfig);
+
+    // Assert
+    expect(options.pilets.length).toEqual(providedPilets.length);
+
+    if(wasUndefined) {
+      process.env.DEBUG_PIRAL = undefined;
+    }
   });
 
   it('createPiletOptions creates the options exposing the global dependencies', () => {
-    const options = createPiletOptions({
-      availablePilets: [],
-      context: {},
-      createApi: () => ({}),
-    });
+
+    // Arrange
+    const setupMock = jest.fn();
+    const globalContext = createMockContainer().context
+    const providedPilets: Array<Pilet> = [
+      {
+        setup: setupMock,
+        hash: '12g',
+        name: 'somePilet',
+        version: '1',
+      },
+      {
+        setup: setupMock,
+        hash: '99a',
+        name: 'anotherPilet',
+        version: '2',
+      },
+    ];
+    const optionsConfig: PiletOptionsConfig = {
+      createApi: createMockApi,
+      availablePilets: providedPilets,
+      context: globalContext,
+      fetchDependency: jest.fn(),
+      getDependencies: () => globalDependencies,
+      loadPilet: jest.fn(),
+      requestPilets: jest.fn(() => Promise.resolve(providedPilets)),
+      strategy: jest.fn()
+    }
+
+    // Act
+    const options = createPiletOptions(optionsConfig);
+
+    // Assert
     expect(options.dependencies).toEqual(globalDependencies);
   });
 
   it('createPiletOptions creates the options with provided requestPilets', () => {
-    const requestPilets = jest.fn();
-    const options = createPiletOptions({
-      availablePilets: [],
-      context: {},
-      createApi: () => ({}),
-      requestPilets,
-    });
-    expect(requestPilets).not.toHaveBeenCalled();
+    // const requestPilets = jest.fn();
+    // const options = createPiletOptions({
+    //   availablePilets: [],
+    //   context: {},
+    //   createApi: () => ({}),
+    //   requestPilets,
+    // });
+    // expect(requestPilets).not.toHaveBeenCalled();
+    // options.fetchPilets();
+    // expect(requestPilets).toHaveBeenCalled();
+
+    const wasUndefined = process.env.DEBUG_PIRAL === undefined;
+
+    // Arrange
+    process.env.DEBUG_PIRAL = "true";
+    const setupMock = jest.fn();
+    const requestPilets = jest.fn(() => Promise.resolve(providedPilets));
+    const globalContext = createMockContainer().context
+    const providedPilets: Array<Pilet> = [
+      {
+        setup: setupMock,
+        hash: '12g',
+        name: 'somePilet',
+        version: '1',
+      },
+      {
+        setup: setupMock,
+        hash: '99a',
+        name: 'anotherPilet',
+        version: '2',
+      },
+    ];
+    const optionsConfig: PiletOptionsConfig = {
+      createApi: createMockApi,
+      availablePilets: providedPilets,
+      context: globalContext,
+      fetchDependency: jest.fn(),
+      getDependencies: jest.fn(),
+      loadPilet: jest.fn(),
+      requestPilets: requestPilets,
+      strategy: jest.fn()
+    }
+
+    // Act
+    const options = createPiletOptions(optionsConfig);
     options.fetchPilets();
+
+    // Assert
     expect(requestPilets).toHaveBeenCalled();
+
+    if(wasUndefined) {
+      process.env.DEBUG_PIRAL = undefined;
+    }
   });
 });
