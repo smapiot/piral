7cd3771a6c95ffbc703992edd1dcc4f789b0707c
Finn Fiedler <finn.fiedler@smapiot.com>
2020-07-08 09:51:32 +0200

    improved test coverage for pilet injector

--- src/tooling/piral-cli/src/injectors/pilet.test.ts
+++ src/tooling/piral-cli/src/injectors/pilet.test.ts
@@ -1,5 +1,6 @@
 import PiletInjector from './pilet';
 import { EventEmitter } from 'events';
+import { KrasRequest } from 'kras';
 
 describe('Piral-CLI piral injector', () => {
   it('PiletInjector is active when configured', () => {
@@ -13,5 +14,152 @@ describe('Piral-CLI piral injector', () => {
     const core = new EventEmitter();
     const injector = new PiletInjector(config, undefined, core);
     expect(injector.active).toBeTruthy();
+  });
+
+  it('PiletInjector properties can be accessed', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: '',
+      app: '',
+      active: true,
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+
+    // Act
+    injector.active = false;
+    injector.name;
+    injector.getOptions();
+    //injector.getMetaOf(0);
+    injector.getMeta();
+
+    // Assert
+    expect(injector.active).toBeFalsy();
+  });
+
+  it('PiletInjector does not send empty content', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: '',
+      app: '',
+      active: true,
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+    const target = '';
+    const url = '';
+
+    let hasFailed = false;
+
+    // Act
+    try {
+      injector.sendFile(target, url); // this file does not exist
+    } catch {
+      hasFailed = true;
+    }
+    // Assert
+    expect(hasFailed).toBeTruthy();
+  });
+
+  it('PiletInjector does not crash when sending invalid content', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: '',
+      app: '',
+      active: true,
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+
+    // Act
+    const res = injector.sendContent('someContent', 'application/json', 'invalidUrl');
+
+    // Assert
+    expect(res).not.toBeUndefined();
+  });
+
+  it('PiletInjector can send reponse and fails with invalid path', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: '',
+      app: '',
+      active: true,
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+
+    // Act
+    const res = injector.sendResponse('some/nice/invalid/path', 'localhost:1234');
+
+    // Assert
+    expect(res).toBeUndefined();
+  });
+
+  it('PiletInjector wont crash on mocked request', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: 'http://someFakeApi:1234',
+      app: '',
+      active: true
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+    const request: KrasRequest = {
+      content: 'someFakeContent',
+      headers: {},
+      method: 'PUT',
+      query: {},
+      target: config.api,
+      url: 'localhost:1234'
+    }
+
+    // Act
+    const res = injector.handle(request);
+
+    // Assert
+    expect(res).toBeUndefined();
+  });
+
+  it('PiletInjector wont crash on request with no target', () => {
+
+    // Arrange
+    const config = {
+      pilets: [],
+      port: 1234,
+      api: 'http://someFakeApi:1234',
+      app: '',
+      active: true,
+    };
+    const core = new EventEmitter();
+    const injector = new PiletInjector(config, undefined, core);
+    const request: KrasRequest = {
+      content: 'someFakeContent',
+      headers: {},
+      method: 'PUT',
+      query: {},
+      target: '',
+      url: 'localhost:1234'
+    }
+
+    // Act
+    const res = injector.handle(request);
+
+    // Assert
+    expect(res).toBeUndefined();
   });
 });
