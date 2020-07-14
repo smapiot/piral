81b92402a2a96cc105097ab913645b724a23a602
Finn <26823828+int0x81@users.noreply.github.com>
2020-07-14 09:31:15 +0200

    Created unit test for .npmrc file creation

--- src/tooling/piral-cli/src/apps/new-pilet.test.ts
+++ src/tooling/piral-cli/src/apps/new-pilet.test.ts
@@ -39,6 +39,20 @@ describe('New Pilet Command', () => {
     // Assert
     expect(result).toBeUndefined();
   });
+  
+  it('should scaffold without creating npmrc file', async () => {
+    jest.setTimeout(60000);
+    const dir = createTempDir();
+    await newPilet(dir, {
+      install: false,
+      source: 'piral@latest',
+    });
+    expect(existsSync(resolve(dir, 'node_modules/piral/package.json'))).toBeTruthy();
+    expect(existsSync(resolve(dir, 'package.json'))).toBeTruthy();
+    expect(existsSync(resolve(dir, 'tsconfig.json'))).toBeTruthy();
+    expect(existsSync(resolve(dir, 'src/index.tsx'))).toBeTruthy();
+    expect(existsSync(resolve(dir, '.npmrc'))).toBeFalsy();
+  });
 
   it('scaffolding with language JS works', async () => {
     jest.setTimeout(60000);
