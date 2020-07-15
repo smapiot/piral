6a0453644d6998f4eb217e2ffbd33dbf6c9262d9
Jens Thirmeyer <jens@thirmeyer.de>
2020-07-15 16:12:12 +0200

    Added tests to Piral CLI component http

--- src/tooling/piral-cli/src/common/http.test.ts
+++ src/tooling/piral-cli/src/common/http.test.ts
@@ -1,6 +1,11 @@
-import { postFile } from './http';
+import { postFile, downloadFile } from './http';
 
 const apiUrl = 'http://sample.fooo.com/api/v1/pilet';
+
+let errorRequest = false;
+let errorOther = false;
+let errorResponse = false;
+let errorResponse2 = false;
 
 jest.mock('axios', () => ({
   default: {
@@ -8,7 +13,31 @@ jest.mock('axios', () => ({
       const found = url === apiUrl;
       const auth = options.headers.authorization === 'Basic 123';
 
-      if (!found) {
+      if (errorRequest) {
+        return Promise.reject({
+          request: {},
+        });
+      } else if (errorOther) {
+        return Promise.reject({
+          message: 'error',
+        });
+      } else if (errorResponse) {
+        return Promise.reject({
+          response: {
+            status: 410,
+            statusText: 'Not Gone',
+            data: '{ "message": "This component is not available anymore." }',
+          },
+        });
+      } else if (errorResponse2) {
+        return Promise.reject({
+          response: {
+            status: 410,
+            statusText: 'Not Gone',
+            data: { message: 'This component is not available anymore.' },
+          },
+        });
+      } else if (!found) {
         return Promise.reject({
           response: {
             status: 404,
@@ -29,6 +58,14 @@ jest.mock('axios', () => ({
         });
       }
     },
+    get(url, options) {
+      if (errorOther) {
+        return Promise.reject({
+          message: 'error',
+        });
+      }
+      return Promise.resolve({ data: 'test' });
+    },
   },
 }));
 
@@ -46,5 +83,39 @@ describe('HTTP Module', () => {
   it('postFile form not found to post file should be false', async () => {
     const result = await postFile('http://sample.com/', '', Buffer.from('example'));
     expect(result).toBeFalsy();
+  });
+
+  it('postFile call results in error request', async () => {
+    errorRequest = true;
+    const result = await postFile('http://sample.com/', '', Buffer.from('example'));
+    expect(result).toBeFalsy();
+    errorRequest = false;
+  });
+
+  it('postFile call results in error other', async () => {
+    errorOther = true;
+    const result = await postFile('http://sample.com/', '', Buffer.from('example'));
+    expect(result).toBeFalsy();
+    errorOther = false;
+  });
+
+  it('postFile call results in error response', async () => {
+    errorResponse = true;
+    let result = await postFile('http://sample.com/', '', Buffer.from('example'));
+    expect(result).toBeFalsy();
+    errorResponse = false;
+    errorResponse2 = true;
+    result = await postFile('http://sample.com/', '', Buffer.from('example'));
+    expect(result).toBeFalsy();
+    errorResponse2 = false;
+  });
+
+  it('downloadFile calls results in error', async () => {
+    errorOther = true;
+    let result = await downloadFile('http://sample.com/', Buffer.from('example'));
+    expect(result.length).toBe(0);
+    errorOther = false;
+    result = await downloadFile('http://sample.com/', Buffer.from('example'));
+    expect(result.length).toBe(0);
   });
 });
