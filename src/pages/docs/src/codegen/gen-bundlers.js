3acd594cc34f1e90e631f1e707859bf778f0eb54
Finn Fiedler <finn.fiedler@smapiot.com>
2020-09-14 14:23:13 +0200

    cross platforms improvements

--- src/pages/docs/src/codegen/gen-bundlers.js
+++ src/pages/docs/src/codegen/gen-bundlers.js
@@ -1,9 +1,19 @@
 const { generated, getName, getBundlers } = require('./paths');
 const { render } = require('./markdown');
 const { generateStandardPage } = require('./pages');
+const { sep } = require('path');
 
 function getRoute(name) {
   return (name && `/tooling/${name}`) || '';
+}
+
+/**
+ * Takes a path and seperates it into single parts
+ * @param filePath The path in form of a string
+ */
+function getPathElements(filePath) {
+
+  return filePath.split(sep);
 }
 
 module.exports = function() {
@@ -12,8 +22,8 @@ module.exports = function() {
   const imports = bundlers.map(file => {
 
     const { mdValue, meta = {} } = render(file, generated);
-    const pathElements = file.split('\\').length;
-    const nameToUse = file.split('\\')[pathElements - 2];
+    const pathElements = getPathElements(file);
+    const nameToUse = pathElements[pathElements.length - 2];
     const route = getRoute(nameToUse);
     const pageMeta = {
       ...meta,
