90031c57785ffcfc8525610a2c73444d7a049d38
Florian Rappl <florian.rappl@smapiot.com>
2020-10-06 18:15:29 +0200

    Enhanced emoji handling

--- src/tooling/piral-cli/src/common/emoji.ts
+++ src/tooling/piral-cli/src/common/emoji.ts
@@ -2,5 +2,5 @@ import { isWindows } from './info';
 
 const supportsEmoji = !isWindows || process.env.TERM === 'xterm-256color';
 
-export const liveIcon = supportsEmoji ? '⚡️' : '>';
-export const settingsIcon = supportsEmoji ? '⚙️' : '>';
+export const liveIcon = supportsEmoji ? '🚀' : '>';
+export const settingsIcon = supportsEmoji ? '🔧' : '>';
