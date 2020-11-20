8af97b82a47583ff6c427ed94499df308149963e
Florian Rappl <florian.rappl@smapiot.com>
2020-11-20 12:18:14 +0100

    Added tests for disposer

--- src/plugins/piral-breadcrumbs/src/create.test.ts
+++ src/plugins/piral-breadcrumbs/src/create.test.ts
@@ -18,6 +18,11 @@ function createMockContainer() {
   };
 }
 
+function createApi(container) {
+  Object.assign(container.api, (createBreadcrumbsApi()(container.context) as any)(container.api, moduleMetadata));
+  return container.api;
+}
+
 const moduleMetadata = {
   name: 'my-module',
   version: '1.0.0',
@@ -27,11 +32,11 @@ const moduleMetadata = {
 };
 
 describe('Create Breadcrumb API Extensions', () => {
-  it('createCoreApi can register and unregister a breadcrumb', () => {
+  it('createBreadcrumbsApi can register and unregister a breadcrumb', () => {
     const container = createMockContainer();
     container.context.registerBreadcrumb = jest.fn();
     container.context.unregisterBreadcrumb = jest.fn();
-    const api = (createBreadcrumbsApi()(container.context) as any)(container.api, moduleMetadata);
+    const api = createApi(container);
     api.registerBreadcrumb('my-bc', {
       title: 'My breadcrumb',
       path: '/example',
@@ -39,6 +44,38 @@ describe('Create Breadcrumb API Extensions', () => {
     expect(container.context.registerBreadcrumb).toHaveBeenCalledTimes(1);
     expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(0);
     api.unregisterBreadcrumb('my-bc');
+    expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterBreadcrumb.mock.calls[0][0]).toBe(container.context.registerBreadcrumb.mock.calls[0][0]);
+  });
+
+  it('createBreadcrumbsApi can dispose a registered breadcrumb', () => {
+    const container = createMockContainer();
+    container.context.registerBreadcrumb = jest.fn();
+    container.context.unregisterBreadcrumb = jest.fn();
+    const api = createApi(container);
+    const dispose = api.registerBreadcrumb('my-bc', {
+      title: 'My breadcrumb',
+      path: '/example',
+    });
+    expect(container.context.registerBreadcrumb).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(0);
+    dispose();
+    expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterBreadcrumb.mock.calls[0][0]).toBe(container.context.registerBreadcrumb.mock.calls[0][0]);
+  });
+
+  it('createBreadcrumbsApi can dispose a registered anonymous breadcrumb', () => {
+    const container = createMockContainer();
+    container.context.registerBreadcrumb = jest.fn();
+    container.context.unregisterBreadcrumb = jest.fn();
+    const api = createApi(container);
+    const dispose = api.registerBreadcrumb({
+      title: 'My breadcrumb',
+      path: '/example',
+    });
+    expect(container.context.registerBreadcrumb).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(0);
+    dispose();
     expect(container.context.unregisterBreadcrumb).toHaveBeenCalledTimes(1);
     expect(container.context.unregisterBreadcrumb.mock.calls[0][0]).toBe(container.context.registerBreadcrumb.mock.calls[0][0]);
   });
--- src/plugins/piral-dashboard/src/create.test.ts
+++ src/plugins/piral-dashboard/src/create.test.ts
@@ -1,8 +1,8 @@
 import { Atom, swap } from '@dbeining/react-atom';
-import { createElement, SFC } from 'react';
+import { createElement, FC } from 'react';
 import { createDashboardApi } from './create';
 
-const StubComponent: SFC = (props) => createElement('div', props);
+const StubComponent: FC = (props) => createElement('div', props);
 StubComponent.displayName = 'StubComponent';
 
 function createMockContainer() {
@@ -22,6 +22,11 @@ function createMockContainer() {
   };
 }
 
+function createApi(container) {
+  Object.assign(container.api, (createDashboardApi()(container.context) as any)(container.api, moduleMetadata));
+  return container.api;
+}
+
 const moduleMetadata = {
   name: 'my-module',
   version: '1.0.0',
@@ -31,15 +36,41 @@ const moduleMetadata = {
 };
 
 describe('Create Dashboard API Extensions', () => {
-  it('createCoreApi can register and unregister a tile', () => {
+  it('createDashboardApi can register and unregister a tile', () => {
     const container = createMockContainer();
     container.context.registerTile = jest.fn();
     container.context.unregisterTile = jest.fn();
-    const api = (createDashboardApi()(container.context) as any)(container.api, moduleMetadata);
+    const api = createApi(container);
     api.registerTile('my-tile', StubComponent);
     expect(container.context.registerTile).toHaveBeenCalledTimes(1);
     expect(container.context.unregisterTile).toHaveBeenCalledTimes(0);
     api.unregisterTile('my-tile');
+    expect(container.context.unregisterTile).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterTile.mock.calls[0][0]).toBe(container.context.registerTile.mock.calls[0][0]);
+  });
+  
+  it('createDashboardApi can dispose a registered tile', () => {
+    const container = createMockContainer();
+    container.context.registerTile = jest.fn();
+    container.context.unregisterTile = jest.fn();
+    const api = createApi(container);
+    const dispose = api.registerTile('my-tile', StubComponent);
+    expect(container.context.registerTile).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterTile).toHaveBeenCalledTimes(0);
+    dispose();
+    expect(container.context.unregisterTile).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterTile.mock.calls[0][0]).toBe(container.context.registerTile.mock.calls[0][0]);
+  });
+  
+  it('createDashboardApi can dispose an anonymous tile', () => {
+    const container = createMockContainer();
+    container.context.registerTile = jest.fn();
+    container.context.unregisterTile = jest.fn();
+    const api = createApi(container);
+    const dispose = api.registerTile(StubComponent);
+    expect(container.context.registerTile).toHaveBeenCalledTimes(1);
+    expect(container.context.unregisterTile).toHaveBeenCalledTimes(0);
+    dispose();
     expect(container.context.unregisterTile).toHaveBeenCalledTimes(1);
     expect(container.context.unregisterTile.mock.calls[0][0]).toBe(container.context.registerTile.mock.calls[0][0]);
   });
