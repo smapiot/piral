94fc8c2d96b1b5e4c79f50a18815c02071d6ac2b
Florian Rappl <florian.rappl@smapiot.com>
2021-10-12 13:30:11 +0200

    Fixed parcel import issue

--- src/tooling/piral-cli-parcel/src/parcel/bundler.ts
+++ src/tooling/piral-cli-parcel/src/parcel/bundler.ts
@@ -93,10 +93,11 @@ export function gatherJsBundles(
 
 function gatherExternals(bundle: Bundler.ParcelBundle, gatheredExternals: Array<string> = []) {
   if (bundle.type === 'external') {
-    const parts = bundle.name.split('/').pop().split('.');
-    parts.pop();
-    parts.pop();
-    gatheredExternals.push(parts.join('.'));
+    const offset = 1;
+    const length = '.external'.length + offset;
+    const assetName = bundle.entryAsset.name;
+    const name = assetName.substr(offset, assetName.length - length);
+    gatheredExternals.push(name);
   }
 
   for (const childBundle of bundle.childBundles) {
