430254ac86d721dcd980df8d17024d468096d1ba
Majd-sufian <majdnewsufian@gmail.com>
2022-03-17 09:16:23 +0100

    Added more tests to improve test coverage in createInstance

--- src/framework/piral-core/src/createInstance.test.tsx
+++ src/framework/piral-core/src/createInstance.test.tsx
@@ -2,7 +2,7 @@ import { createInstance } from './createInstance';
 import { standardStrategy, blazingStrategy } from 'piral-base';
 
 describe('Piral-Core createInstance module', () => {
-  it('createInstance without arguments uses the standard strategy', () => {
+  it('createInstance without arguments uses the standard', () => {
     const instance = createInstance();
     expect(instance.options.strategy).toBe(standardStrategy);
   });
@@ -13,7 +13,12 @@ describe('Piral-Core createInstance module', () => {
   });
 
   it('createInstance with empty actions uses the standard strategy', () => {
-    const instance = createInstance({ actions: {} });
+    const instance = createInstance({ plugins: {}, actions: {} });
     expect(instance.options.strategy).toBe(standardStrategy);
+  })
+
+  it('createInstance with async function uses the blazing strategy', () => {
+    const instance = createInstance({ async: () => { } });
+    expect(instance.options.strategy).toBe(blazingStrategy);
   })
 });
