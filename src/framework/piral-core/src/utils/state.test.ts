a4fabd23aa2a8bf32191de3c420fac4d6de7127d
Majd-sufian <majdnewsufian@gmail.com>
2022-03-18 12:52:48 +0100

    Improved tests in state and setter

--- src/framework/piral-core/src/utils/state.test.ts
+++ src/framework/piral-core/src/utils/state.test.ts
@@ -1,14 +1,7 @@
-import { withAll, withRootExtension } from './state';
+import { withRootExtension } from './state';
 import { RootListener } from '../RootListener';
-import { Atom } from '@dbeining/react-atom';
-import { GlobalState } from '../types';
 
 describe('State Module', () => {
-  it('withAll should return all state with dispatchers', () => {
-    const result = withAll(state);
-    expect(result).not.toEqual({});
-  });
-
   it('withRootExtension should create an extension', () => {
     const dispatch = withRootExtension('testNmae', RootListener);
     const result = dispatch({ registry: { extensions: {} } });
