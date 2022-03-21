57cf05c589ae22709e65bd0f8e2b03acad682c38
Florian Rappl <rappl.florian@gmail.com>
2022-03-21 18:06:10 +0100

    Updated provided element.ts tests

--- src/framework/piral-core/src/modules/element.test.ts
+++ src/framework/piral-core/src/modules/element.test.ts
@@ -3,35 +3,45 @@ import { createListener } from 'piral-base';
 import { createActions } from '../state';
 import { renderElement } from './element';
 
-describe('Elemnts Module', () => {
+describe('Elements Module', () => {
   const state = Atom.of({
     portals: {},
   });
   const context = createActions(state, createListener({}));
 
-  it('renderElement', () => {
+  it('testing basic renderElement functionality of piral-extension web component', () => {
     const element = document.createElement('piral-extension');
+    const event = new Event('extension-props-changed');
     document.body.appendChild(element);
+    renderElement(context, element, {});
     element.setAttribute('name', 'foo');
     element.setAttribute('params', {});
-    document.body.removeChild(element);
-    renderElement(context, element, {});
-    const event = new Event('extension-props-changed');
     element.dispatchEvent(event);
+    renderElement(context, element, {});
+    document.body.removeChild(element);
+    expect(deref(state)['portals']['root'].length).toBe(1);
+  });
+
+  it('disposing piral-extension web component', () => {
+    const element = document.createElement('piral-extension');
+    document.body.appendChild(element);
+    renderElement(context, element, {});
     const [dispose] = renderElement(context, element, {});
     dispose();
     expect(deref(state)['portals']['root'].length).toBe(1);
   });
 
-  it('testing setters and getters in class PiralExtension', () => {
+  it('testing setters and getters in piral-extension web component', () => {
     const element = document.createElement('piral-extension');
     document.body.appendChild(element);
-    element.params;
+    expect(element.params).toEqual({});
+    expect(element.name).toEqual(undefined);
+    expect(element.empty).toEqual(undefined);
     element.params = 'anything';
-    expect(element.params).toEqual('anything');
     element.name = 'foo';
-    expect(element.name).toEqual('foo');
     element.empty = 'anything';
+    expect(element.params).toEqual('anything');
+    expect(element.name).toEqual('foo');
     expect(element.empty).toEqual('anything');
   });
 });
