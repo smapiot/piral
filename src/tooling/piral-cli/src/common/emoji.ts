dfcca9c36d23c47748041694c059b572f1f43e6f
Florian Rappl <florian.rappl@smapiot.com>
2022-06-23 15:55:38 +0200

    More emojis

--- src/tooling/piral-cli/src/common/emoji.ts
+++ src/tooling/piral-cli/src/common/emoji.ts
@@ -4,3 +4,12 @@ const supportsEmoji = !isWindows || process.env.TERM === 'xterm-256color';
 
 export const liveIcon = supportsEmoji ? '🚀 ' : '>';
 export const settingsIcon = supportsEmoji ? '🔧 ' : '>';
+// see https://unicode.org/emoji/charts/full-emoji-list.html
+export const cactusIcon = supportsEmoji ? '👻' : '';
+export const clapIcon = supportsEmoji ? '👏' : '';
+export const sparklesIcon = supportsEmoji ? '✨' : '';
+export const unicornIcon = supportsEmoji ? '🦄' : '';
+export const caterpillerIcon = supportsEmoji ? '🐛' : '';
+export const butterflyIcon = supportsEmoji ? '🦋' : '';
+export const zapIcon = supportsEmoji ? '⚡' : '';
+export const rainbowIcon = supportsEmoji ? '🌈' : '';
