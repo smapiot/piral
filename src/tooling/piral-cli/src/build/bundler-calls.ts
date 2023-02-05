8833e0913487f5b11446b6dfdf5354e9154cbded
Florian Rappl <florian.rappl@smapiot.com>
2023-02-05 20:30:40 +0100

    Do not end stream from subprocess

--- src/tooling/piral-cli/src/build/bundler-calls.ts
+++ src/tooling/piral-cli/src/build/bundler-calls.ts
@@ -71,9 +71,9 @@ export function callDynamic<T extends BaseBundleParameters>(name: string, path: 
       });
     };
 
-    ps.stderr && process.stderr && ps.stderr.pipe(process.stderr);
-    ps.stdout && process.stdout && ps.stdout.pipe(process.stdout);
-    ps.stdin && process.stdin && ps.stdin.pipe(process.stdin);
+    ps.stderr && process.stderr && ps.stderr.pipe(process.stderr, { end: false });
+    ps.stdout && process.stdout && ps.stdout.pipe(process.stdout, { end: false });
+    ps.stdin && process.stdin && ps.stdin.pipe(process.stdin, { end: false });
 
     ps.on('message', (msg: any) => {
       switch (msg.type) {
@@ -113,9 +113,9 @@ export function callStatic<T extends BaseBundleParameters>(name: string, path: s
       });
     };
 
-    ps.stderr && process.stderr && ps.stderr.pipe(process.stderr);
-    ps.stdout && process.stdout && ps.stdout.pipe(process.stdout);
-    ps.stdin && process.stdin && ps.stdin.pipe(process.stdin);
+    ps.stderr && process.stderr && ps.stderr.pipe(process.stderr, { end: false });
+    ps.stdout && process.stdout && ps.stdout.pipe(process.stdout, { end: false });
+    ps.stdin && process.stdin && ps.stdin.pipe(process.stdin, { end: false });
 
     ps.on('message', (msg: any) => {
       switch (msg.type) {
