4bbbe110222dfef274b1e3f8d574313b905adc2d
Florian Rappl <florian.rappl@smapiot.com>
2023-02-09 13:54:24 +0100

    Added usePiletApi hook

---
+++ src/framework/piral-core/src/hooks/piletApi.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useGlobalStateContext } from "./globalState";
+
+/**
+ * Uses the global state context to retrieve the pilet API stored
+ * for the root pilet. This allows interaction with the pilet API
+ * from the app shell.
+ * @returns The pilet API object of the root pilet.
+ */
+export function usePiletApi() {
+  const context = useGlobalStateContext();
+  const [api] = useState(() => Object.values(context.apis).shift());
+  return api;
+}
