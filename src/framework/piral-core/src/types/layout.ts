0d07048816a9d439792ef1809dda10576a75044a
Florian Rappl <florian.rappl@smapiot.com>
2025-03-19 11:23:31 +0100

    Update src/framework/piral-core/src/types/layout.ts
    Co-authored-by: Manuel Römer <30902964+manuelroemer@users.noreply.github.com>

--- src/framework/piral-core/src/types/layout.ts
+++ src/framework/piral-core/src/types/layout.ts
@@ -2,6 +2,9 @@
  * The mapping of breakpoints to layout type index.
  * Choose up to three breakpoints.
  * In case of no breakpoint a default layout will be chosen.
+ 
+ @example
+ `['(min-width: 991px)', '(min-width: 481px)', '(max-width: 480px)']` for desktop, tablet, mobile.
  */
 export type LayoutBreakpoints = [string?, string?, string?];
 
